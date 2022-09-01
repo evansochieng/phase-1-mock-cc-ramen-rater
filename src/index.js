ee00721b21ba61efaeecba20611ea991cca5e850
Evans Ochieng <evansochieng121@gmail.com>
2022-09-01 12:20:27 +0300

    Display new ramen

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Display ramen images
     displayRamenImages()
+
+    // Display new ramens
+    displayNewRamen();
 })
 
 // Display all the ramen images on the DOM
@@ -39,7 +42,7 @@ function displayRamenDetails(ramen){
     const rating = ramen.rating;
     const comment = ramen.comment;
 
-    // Append thge info to the DOM
+    // Append the info to the DOM
     // Add image
     const ramenImage = document.querySelector(".detail-image");
     ramenImage.src = image;
@@ -59,4 +62,53 @@ function displayRamenDetails(ramen){
     // Add comment
     const ramenComment = document.querySelector('#comment-display');
     ramenComment.textContent = comment;
+}
+
+// Display new ramen 
+function displayNewRamen(){
+    const form = document.querySelector('#new-ramen');
+
+    // Listen to the user submission
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        // Grab the details of the new ramen
+        const newRamenName = event.target["new-name"].value;
+        const newRamenRestaurant = event.target["new-restaurant"].value;
+        const newRamenImage = event.target['new-image'].value;
+        const newRamenRating = event.target['new-rating'].value;
+        const newRamenComment = event.target['new-comment'].value;
+        
+        // Add this ramen image to the images' div
+        const img = document.createElement('img');
+        img.src = newRamenImage;
+        // Grab the div for the images
+        const imgDiv = document.querySelector('#ramen-menu');
+        imgDiv.appendChild(img);
+
+        // Add listen to the click event on the ramen image
+        // Display ramen details on click
+        img.addEventListener('click', () => {
+            // Append the info to the DOM
+            // Add image
+            const ramenImage = document.querySelector(".detail-image");
+            ramenImage.src = newRamenImage;
+
+            // Add name
+            const ramenName = document.querySelector('.name');
+            ramenName.textContent = newRamenName;
+
+            // Add restaurant
+            const ramenRestaurant = document.querySelector('.restaurant');
+            ramenRestaurant.innerHTML = newRamenRestaurant;
+
+            // Add rating
+            const ramenRating = document.querySelector('#rating-display');
+            ramenRating.innerText = newRamenRating;
+
+            // Add comment
+            const ramenComment = document.querySelector('#comment-display');
+            ramenComment.textContent = newRamenComment;
+        })
+    })
 }
