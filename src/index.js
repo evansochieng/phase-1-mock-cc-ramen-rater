7d8c676884d5d4e3942094e24a3a4953176d63d1
Evans Ochieng <evansochieng121@gmail.com>
2022-09-01 10:49:25 +0300

    Displayed ramen images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+
+// Fetch data from the local API and update the DOM
+// Listen to user events and update the DOM
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Display ramen images
+    displayImages()
+})
+
+// Display all the ramen images on the DOM
+function displayImages(){
+    fetch('http://localhost:3000/ramens')
+    .then( (response) => response.json())
+    .then( (result) => {
+        // Grab the div for the images
+        const imgDiv = document.querySelector('#ramen-menu');
+        // Add the images to the div
+        for (let ramen of result){
+            const img = document.createElement('img');
+            img.src = ramen.image;
+            imgDiv.appendChild(img);
+        }
+    })
+}
