e8274351f415968c0e855c63dac6dca488ca781f
Evans Ochieng <evansochieng121@gmail.com>
2022-09-01 11:16:15 +0300

    Displayed ramen details

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // Display ramen images
-    displayImages()
+    displayRamenImages()
 })
 
 // Display all the ramen images on the DOM
-function displayImages(){
+function displayRamenImages(){
     fetch('http://localhost:3000/ramens')
     .then( (response) => response.json())
     .then( (result) => {
@@ -20,6 +20,43 @@ function displayImages(){
             const img = document.createElement('img');
             img.src = ramen.image;
             imgDiv.appendChild(img);
+
+            // Listen to the click event on the image
+            // Display that ramen's details
+            img.addEventListener('click', () => {
+                displayRamenDetails(ramen);
+            })
         }
     })
+}
+
+// Display information about a ramen when clicked
+function displayRamenDetails(ramen){
+    // Capture ramen info
+    const name = ramen.name;
+    const restaurant = ramen.restaurant;
+    const image = ramen.image;
+    const rating = ramen.rating;
+    const comment = ramen.comment;
+
+    // Append thge info to the DOM
+    // Add image
+    const ramenImage = document.querySelector(".detail-image");
+    ramenImage.src = image;
+
+    // Add name
+    const ramenName = document.querySelector('.name');
+    ramenName.textContent = name;
+
+    // Add restaurant
+    const ramenRestaurant = document.querySelector('.restaurant');
+    ramenRestaurant.innerHTML = restaurant;
+
+    // Add rating
+    const ramenRating = document.querySelector('#rating-display');
+    ramenRating.innerText = rating;
+
+    // Add comment
+    const ramenComment = document.querySelector('#comment-display');
+    ramenComment.textContent = comment;
 }
